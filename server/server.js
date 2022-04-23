4129dca3778d0072f68bf25f7abd2a5c6bc942f1
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-04-22 20:55:13 -0500

    revisions to package.json on client side; server/server.js slight revision

--- server/server.js
+++ server/server.js
@@ -25,6 +25,21 @@ startServer()
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  // First, we check to see if the Node environment is in production. 
+  // If it is, we instruct the Express.js server to serve any files in the React application's 
+  // build directory in the client folder.
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+// wildcard GET route for the server: if we make a GET request to any location on the server 
+// that doesn't have an explicit route defined, respond with the production-ready React front-end code.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+// run server on port specified by connection.js (either MongoDB uri or localhost)
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
